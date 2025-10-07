59ebffe4622b3c7305827358cc5fd145abb94984
jinwoo fung <30623585+jnwof@users.noreply.github.com>
2025-10-07 14:37:48 +0900

    added IF NOT EXISTS cond

---
+++ init_db.sql
@@ -0,0 +1,23 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- schema v0 
+CREATE TABLE IF NOT EXISTS users (
+    user_id UUID DEFAULT gen_random_uuid(),
+    username VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS workouts (
+    workout_id UUID DEFAULT gen_random_uuid(), 
+	user_id UUID NOT NULL, 
+	date DATE NOT NULL,
+	split VARCHAR(25) NOT NULL,
+	workout_details JSON NOT NULL,
+    PRIMARY KEY(workout_id),
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(user_id)
+); 
+
+CREATE INDEX index_user_id_date ON workouts(user_id, date); 
